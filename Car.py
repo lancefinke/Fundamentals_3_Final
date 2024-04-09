92148fe4fca1a6b6fc9d461628590395780e0e79
Lance <lancefinke@icloud.com>
2024-04-09 13:52:25 -0500

    e

--- Car.py
+++ Car.py
@@ -31,5 +31,10 @@ class Car:
     def setID(self,newID):self.ID=newID
     
 
+c1 = Car("Ford","Mustang","2014","red",51423,"manual",1)
+
+print(c1.getMiles())
+c1.setMiles(4641)
+print(c1.getMiles())
 
 
---
+++ Catalogue.py
@@ -0,0 +1,7 @@
+import Car as car
+
+c1 = Car("Ford","Mustang","2014","red",51423,"manual",1)
+
+print(c1.getMiles())
+c1.setMiles(4641)
+print(c1.getMiles())
