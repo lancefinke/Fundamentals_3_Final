cf9489de8f5d7162893e17810c657777ff5e248b
Lance <lancefinke@icloud.com>
2024-04-14 14:16:08 -0500

    updated Design Document

--- Catalog.py
+++ Catalog.py
@@ -10,6 +10,7 @@ print(car1.getVerification())
 
 
 
+
 '''api_url = 'https://api.api-ninjas.com/v1/cars?make={}&model={}&year={}'.format("ford","mustang","2014")
 response = requests.get(api_url, headers={'X-Api-Key': 'F+ibc8OExQ83IKX/e3JJmQ==xsvvtTv25O4EXop3'})
 data = response.json()
