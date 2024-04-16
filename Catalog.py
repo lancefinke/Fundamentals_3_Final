6d069d49c4e4931aa0f57970de792334b4b1a7cb
Lance <lancefinke@icloud.com>
2024-04-16 13:51:17 -0500

    start of part 2

--- Catalog.py
+++ Catalog.py
@@ -1,12 +1,20 @@
 from Car import Car
 import requests
+import json
 
 
 
-car1 = Car("toyota","camry","2011","white",95426,5000.00,"automatic",1)
-print(car1.getVerification())
-car1.verify()
-print(car1.getVerification())
+#list of every car in catalog
+car_list = []
+#dictionary that keeps track of the quantities of every make, every model, and every year
+attribute_quantities = {}
+
+
+
+
+
+
+
 
 
 
@@ -14,5 +22,5 @@ print(car1.getVerification())
 '''api_url = 'https://api.api-ninjas.com/v1/cars?make={}&model={}&year={}'.format("ford","mustang","2014")
 response = requests.get(api_url, headers={'X-Api-Key': 'F+ibc8OExQ83IKX/e3JJmQ==xsvvtTv25O4EXop3'})
 data = response.json()
-print(data)'''
+print(data!=[])'''
 
